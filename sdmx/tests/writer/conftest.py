cfcef1708ca7ce0d328383a9fc98db4996f01aba
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-05-04 21:52:50 +0200

    Move writer data to a fixture

---
+++ sdmx/tests/writer/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from sdmx.model import Agency, Code, Codelist
+
+
+@pytest.fixture
+def codelist():
+    ECB = Agency(id='ECB')
+
+    cl = Codelist(
+        id='CL_COLLECTION',
+        version='1.0',
+        is_final=False,
+        is_external_reference=False,
+        maintainer=ECB,
+        name={'en': 'Collection indicator code list'},
+    )
+
+    cl.items['A'] = Code(
+        id='A',
+        name={'en': "Average of observations through period"},
+    )
+    cl.items['B'] = Code(
+        id='B',
+        name={'en': 'Beginning of period'},
+    )
+
+    return cl
--- sdmx/tests/writer/test_writer_xml.py
+++ sdmx/tests/writer/test_writer_xml.py
@@ -1,29 +1,10 @@
 import pytest
 import sdmx
 from sdmx.message import StructureMessage
-from sdmx.model import Agency, Code, Codelist
 
 
-def test_codelist():
-    ECB = Agency(id='ECB')
-    cl = Codelist(
-        id='CL_COLLECTION',
-        version='1.0',
-        is_final=False,
-        is_external_reference=False,
-        maintainer=ECB,
-        name={'en': 'Collection indicator code list'},
-    )
-    cl.items['A'] = Code(
-        id='A',
-        name={'en': "Average of observations through period"},
-    )
-    cl.items['B'] = Code(
-        id='B',
-        name={'en': 'Beginning of period'},
-    )
-
-    sdmx.to_xml(cl)
+def test_codelist(codelist):
+    sdmx.to_xml(codelist)
 
 
 def test_not_implemented():
