ba4c233cdc26b2e31b55621196351bfc7a3e74e6
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-05-18 14:12:27 +0200

    Add test_structure_roundtrip

--- sdmx/tests/writer/test_writer_xml.py
+++ sdmx/tests/writer/test_writer_xml.py
@@ -1,7 +1,13 @@
+import logging
+
 import pytest
 
 import sdmx
 from sdmx.message import DataMessage
+from sdmx.tests.data import specimen
+
+
+log = logging.getLogger(__name__)
 
 
 def test_codelist(tmp_path, codelist):
@@ -23,6 +29,33 @@ def test_structuremessage(tmp_path, structuremessage):
         msg.codelist["CL_COLLECTION"]["A"].name["en"]
         == structuremessage.codelist["CL_COLLECTION"]["A"].name["en"]
     )
+
+    # False because `structuremessage` lacks URNs, which are constructed automatically
+    # by `to_xml`
+    assert not msg.compare(structuremessage, strict=True)
+    # Compares equal when allowing this difference
+    assert msg.compare(structuremessage, strict=False)
+
+
+@pytest.mark.parametrize('specimen_id, strict', [
+    # ('ECB_EXR/1/structure-full.xml', False),
+    # ('ISTAT/47_850-structure.xml', True),
+    ('SGR/common-structure.xml', True),
+])
+def test_structure_roundtrip(specimen_id, strict, tmp_path):
+    # Read a specimen file
+    with specimen(specimen_id) as f:
+        msg0 = sdmx.read_sdmx(f)
+
+    # Write to file
+    path = tmp_path / "output.xml"
+    path.write_bytes(sdmx.to_xml(msg0, pretty_print=True))
+
+    # Read again
+    msg1 = sdmx.read_sdmx(path)
+
+    # Contents are identical
+    assert msg0.compare(msg1, strict), path
 
 
 def test_not_implemented():
