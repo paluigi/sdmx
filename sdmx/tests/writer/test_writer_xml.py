c383493fdef2b8ad3239df538488522a28dc701c
Brock Fanning <brockfanning@gmail.com>
2021-03-06 20:05:57 -0500

    Use more code in tests

--- sdmx/tests/writer/test_writer_xml.py
+++ sdmx/tests/writer/test_writer_xml.py
@@ -10,7 +10,9 @@ from sdmx.model import (
     Dimension,
     Key,
     Observation,
+    SeriesKey,
     StructureSpecificDataSet,
+    PrimaryMeasure,
 )
 
 log = logging.getLogger(__name__)
@@ -76,10 +78,18 @@ def test_ds(dsd, obs):
 
 
 def test_ds_structurespecific(dsd, obs):
-    # Write StructureSpecificDataSet with Observations not in Series
-    ds = StructureSpecificDataSet(structured_by=dsd)
-    ds.obs.append(obs)
-
+    series_key = dsd.make_key(SeriesKey, dict(FOO=1, BAR=2))
+    dimension_key = dsd.make_key(Key, dict(BAZ=3))
+    primary_measure = PrimaryMeasure(id='OBS_VALUE')
+    observation = Observation(
+        series_key=series_key,
+        dimension=dimension_key,
+        value_for=primary_measure,
+        value=25,
+    )
+    series = { series_key: [observation] }
+    ds = StructureSpecificDataSet(structured_by=dsd, series=series)
+    ds.obs.append(observation)
     result = sdmx.to_xml(ds, pretty_print=True)
     print(result.decode())
 
