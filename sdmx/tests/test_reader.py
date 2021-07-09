ac04f56e7a1165af2575ba02ba70f38195c99336
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-07-09 15:41:43 +0200

    Test reader recognition of content types

--- sdmx/tests/test_reader.py
+++ sdmx/tests/test_reader.py
@@ -5,5 +5,7 @@ from sdmx.reader import get_reader_for_content_type
 
 def test_get_reader_for_content_type():
     ctype = "application/x-pdf"
-    with pytest.raises(ValueError, match=f"Unsupported content type: {ctype}"):
+    with pytest.raises(
+        ValueError, match=f"Content type '{ctype}' not supported by any of"
+    ):
         get_reader_for_content_type(ctype)
