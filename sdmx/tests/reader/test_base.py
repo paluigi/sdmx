635dfd07feec89869f5db4cebefe2a2ed764e393
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-07-11 18:04:02 +0200

    Expand coverage in .reader.base

--- sdmx/tests/reader/test_base.py
+++ sdmx/tests/reader/test_base.py
@@ -9,10 +9,20 @@ class TestBaseReader:
         """A reader that implements the minimum abstract methods."""
 
         class cls(BaseReader):
+            content_types = ["application/foo; bar=baz"]
+
             def read_message(self, source, dsd=None):
-                pass
+                pass  # pragma: no cover
 
         return cls
 
     def test_detect(self, MinimalReader):
         assert False is MinimalReader.detect(b"foo")
+
+    def test_supports_content_type(self, caplog, MinimalReader):
+        """:meth:`.support_content_type` matches even when params differ, but logs."""
+        assert True is MinimalReader.supports_content_type("application/foo; bar=qux")
+        assert (
+            "Match application/foo with params {'bar': 'qux'}; expected {'bar': 'baz'}"
+            in caplog.messages
+        )
